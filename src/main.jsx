9635199b9eafd90062bdb4320d26934e07274ce4
HossamElrawy <ho.elrawy98@gmail.com>
2025-05-18 18:24:30 +0300

    f

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css' // Import global styles
 import App from './App.jsx' // Import the main App component
 
+import circlePattern from "../assets/images/pattern-circle.svg"
+import linesPattern from "../assets/images/pattern-lines.svg"
+import topPattern from "../assets/images/pattern-squiggly-line-top.svg"
+import bottomDesktopPattern from "../assets/images/pattern-squiggly-line-bottom-desktop.svg"
+import bottomMobilePattern from "../assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg"
+import logo from "../assets/images/logo-full.svg"
+
 // Create a root React DOM node and render the application
 createRoot(document.getElementById('root')).render(
   // StrictMode helps catch potential problems in the application
@@ -10,17 +17,17 @@ createRoot(document.getElementById('root')).render(
     {/* Main container for all content */}
     <main>
       {/* Decorative background patterns - empty alt tags as they're purely decorative */}
-      <img src="./assets/images/pattern-circle.svg" className="pattern-circle-1" alt="" />
-      <img src="./assets/images/pattern-lines.svg" className="pattern-lines" alt="" />
-      <img src="./assets/images/pattern-squiggly-line-top.svg" className="pattern-top" alt="" />
-      <img src="./assets/images/pattern-squiggly-line-bottom-desktop.svg" className="pattern-bottom-desktop" alt="" />
-      <img src="./assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg" className="pattern-bottom-mobile" alt="" />
-      <img src="./assets/images/pattern-circle.svg" className="pattern-circle-2" alt="" />
+      <img src={circlePattern} className="pattern-circle-1" alt="" />
+      <img src={linesPattern} className="pattern-lines" alt="" />
+      <img src={topPattern} className="pattern-top" alt="" />
+      <img src={bottomDesktopPattern} className="pattern-bottom-desktop" alt="" />
+      <img src={bottomMobilePattern} className="pattern-bottom-mobile" alt="" />
+      <img src={circlePattern} className="pattern-circle-2" alt="" />
 
       {/* Main content container */}
       <div className='main-con'>
         {/* Application logo - consider adding meaningful alt text if this is important content */}
-        <img src="./assets/images/logo-full.svg" className='logo-pic' alt="Conference Ticket Generator Logo" />
+        <img src={logo} className='logo-pic' alt="Conference Ticket Generator Logo" />
         
         {/* The main App component that contains the application logic */}
         <App />
